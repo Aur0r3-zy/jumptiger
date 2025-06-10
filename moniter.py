aeebc824f111769c36b2c8c66a77cedfe79f17c0
Aur0r3-zy <ziyang.dou.cs@gmail.com>
2025-06-10 15:16:32 +0800

    修改控制面板默认IP地址

--- moniter.py
+++ moniter.py
@@ -28,4 +28,4 @@ def fetch_stats():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8088, debug=True)
+    app.run(host="127.0.0.1", port=8088, debug=True)
